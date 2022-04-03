d934b17988ddc20c6a179e9894b7caed0f7a0181
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-04-03 18:45:55 +0530

    final

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonTextContent = this.textContent;
-    switch (buttonTextContent) {
-      case "w":
-        var tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        var tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "s":
-        var tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "d":
-        var tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "j":
-        var snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
-      case "k":
-        var crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
-      case "l":
-        var kick = new Audio("./sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      default:
-        console.log(buttonTextContent);
-        break;
-    }
+    playSound(buttonTextContent);
   });
 }
 
 document.addEventListener("keydown", function (event) {
-  var key = event.key;
+  let key = event.key;
+  playSound(key);
+});
+
+function playSound(key) {
   switch (key) {
     case "w":
       var tom1 = new Audio("./sounds/tom-1.mp3");
@@ -74,4 +45,4 @@ document.addEventListener("keydown", function (event) {
       console.log(key);
       break;
   }
-});
+}
